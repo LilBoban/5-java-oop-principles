e6ed3e24861930e1787ceddf94981dae26011bf8
DmitriiDudarow <dudarow.d@yandex.ru>
2024-10-17 19:59:48 +0500

    Dudarow Task 2

---
+++ task02/src/com/example/task02/DiscountBill.java
@@ -0,0 +1,26 @@
+package com.example.task02;
+
+public class DiscountBill extends Bill {
+
+        private double discount;
+
+        public DiscountBill(double discount) {
+            this.discount = discount;
+        }
+
+        public double getDiscount() {
+            return discount;
+        }
+
+        public long getAbsolutePrice() {
+            return super.getPrice() - getPrice();
+        }
+
+        @Override
+        public long getPrice() {
+            long price = super.getPrice();
+            return (long) (price - price * (discount / 100));
+        }
+
+
+}
--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -10,12 +10,20 @@ public class Task02Main {
     private static final Item ITEM6 = new Item("Товар 6", 60);
 
     public static void main(String[] args) {
-        Bill bill = new Bill();
+        /*Bill bill = new Bill();
         bill.add(ITEM1, 10);
         bill.add(ITEM3, 3);
         bill.add(ITEM6, 1);
         System.out.println(bill);
         bill.add(ITEM3, 3);
-        System.out.println(bill);
+        System.out.println(bill);*/
+
+        DiscountBill discountBill = new DiscountBill(50);
+        discountBill.add(ITEM1, 10);
+        discountBill.add(ITEM4, 3);
+        discountBill.add(ITEM5, 1);
+        System.out.println(discountBill.getAbsolutePrice());
+        System.out.println(discountBill.getDiscount());
+        System.out.println(discountBill);
     }
 }
